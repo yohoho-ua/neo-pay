2f9a3a9d3d0d946d8f5df757ad1602e6de7cf495
yohoho-ua <yohoho.ua@gmail.com>
2018-01-17 17:40:05 +0200

    more tests added: TestInitConfig

--- checkPayment_test.go
+++ checkPayment_test.go
@@ -45,6 +45,21 @@ func TestCheckVouts(t *testing.T) {
 	if actualBalance != expectedBalance {
 		t.Errorf("checkVouts returned unexpected customer balance: got %v want %v", actualBalance, expectedBalance)
 	}
+
 }
+
+func TestInitConfig(t *testing.T) {
+	configuratin := Configuration{NodeURI:"http://localhost:10332"}
+	expectedURI := configuratin.NodeURI
+	actualURI := initConfig().NodeURI
+
+	if actualURI != expectedURI {
+		t.Errorf("initConfig returned unexpected confiuration: got %v want %v", actualURI, expectedURI)
+	}
+
+}
+
+
+
 
 
